8add77df418182b910b2fd410a20ccdb02f306a1
Miles Higbie <miles.higbie@gmail.com>
2019-11-14 02:41:01 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,9 +1,10 @@
 def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
-  puts ""
+  puts "'It's-a me, Mario!"
 end
 
 def toadstool
+  status =""
   puts status
 end
